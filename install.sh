d5a083c24bd9ec1f0b42a3226a51ad569bc4dacf
sguizard <sebastien.guizard@gmail.com>
2015-12-24 15:44:51 +0100

    Correct install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 mkdir /usr/local/share/DensityMap
 cp ./* /usr/local/share/DensityMap
-perl -i -pe 's/.\/color.txt/\/usr\/local\/share\/DensityMap\/color.txt/' /usr/local/share/DensityMap/DensityMap.pl
+perl -i -pe 's/.\/colors.txt/\/usr\/local\/share\/DensityMap\/colors.txt/' /usr/local/share/DensityMap/DensityMap.pl
 cd /usr/local/bin
 ln -s /usr/local/share/DensityMap/DensityMap.pl
 
